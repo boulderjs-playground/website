cdad75b6a1b395ee9e43562429be81da131a9315
Patrick Heneise <patrick@zentered.co>
2025-08-28 18:37:20 -0600

    fix: missing files

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
-import solid from "eslint-plugin-solid/configs/recommended";
+import js from '@eslint/js'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import solid from 'eslint-plugin-solid/configs/recommended'
 
 export default defineConfig([
-  globalIgnores(["package-lock.json"]),
+  globalIgnores(['package-lock.json']),
   js.configs.recommended,
-  solid,
-]);
+  solid
+])
